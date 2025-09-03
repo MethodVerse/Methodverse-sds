c8a5245c67066b9365659bf6102d5d7cb9009e51
Eliyas Zhao <eliyas.zhao@gmail.com>
2025-09-03 13:53:19 -0400

    Work on issue #2, clean up files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
 #include <iostream>
-//#include "parameter_registry.h"
-#include "parameter_defs.h"
-
-using namespace mv;
-// test parameter_defs.h
-DEFINE_TYPED_PARAMETER(TE, "TE", double, "ms");
-DEFINE_TYPED_PARAMETER(TR, "TR", double, "ms");
 
 int main() {
 
-    TE te({10.0, 11.0, 12.0});
-    TR tr(20.0);
     std::cout << "Hello, MethodVerse!" << std::endl;
      
     return 0;
